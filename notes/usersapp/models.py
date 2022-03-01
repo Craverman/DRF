d5226b4f07eb47ddbcec16f07f18b09ed3c476e2
Craverman <CraVerka@googlemail.com>
2022-03-02 01:14:52 +0300

    SuperUser and creating api

---
+++ notes/usersapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class User(AbstractUser):
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField
+    email = models.EmailField(unique=True
