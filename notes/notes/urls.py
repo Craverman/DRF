e47aee601ee0b32d61399a4a6ae38c0c5d80d156
Craverman <CraVerka@googlemail.com>
2022-04-07 01:30:52 +0300

    Tokken authetication and JWT

--- notes/notes/urls.py
+++ notes/notes/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from todo.views import ProjectModelViewSet, TodoModelViewSet
 from usersapp.views import CustomUserModelViewSet
 from rest_framework.authtoken import views
@@ -30,5 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
