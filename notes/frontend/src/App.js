23e3f2034f0da188ebaf19363750d92a761c4f4e
Craverman <CraVerka@googlemail.com>
2022-04-17 15:24:43 +0300

    Lesson_7. Fixing minor issues

--- notes/frontend/src/App.js
+++ notes/frontend/src/App.js
@@ -29,29 +29,6 @@ class App extends React.Component {
             'todos':[]
         }
     }
-    set_token(token) {
-const cookies = new Cookies()
-cookies.set('token', token)
-this.setState({'token': token})
-}
-is_authenticated() {
-return this.state.token != ''
-}
-logout() {
-this.set_token('')
-}
-get_token_from_storage() {
-const cookies = new Cookies()
-const token = cookies.get('token')
-this.setState({'token': token})
-}
-get_token(username, password) {
-axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username,
-password: password})
-.then(response => {
-this.set_token(response.data['token'])
-}).catch(error => alert('Неверный логин или пароль'))
-}
     get_token(username, password) {
         axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username,
             password: password})
