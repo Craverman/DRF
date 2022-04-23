d912d06ca38238473e4fa8cd87ffc7200b586c1a
Craverman <CraVerka@googlemail.com>
2022-04-23 15:32:59 +0300

    Lesson_8. Fixing minor issues

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'usersapp.User'
 
 REST_FRAMEWORK = {
-
+    #
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.DjangoModelPermissions'],
 
