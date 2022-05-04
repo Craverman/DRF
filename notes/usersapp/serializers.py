9fd8e1daca307e035d959a351a2417244eae95ee
Craverman <CraVerka@googlemail.com>
2022-05-05 00:55:11 +0300

    Lesson_9. Adding second api

--- notes/usersapp/serializers.py
+++ notes/usersapp/serializers.py
@@ -6,3 +6,9 @@ class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+
+
+class UserModelSerializerNew(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email']
